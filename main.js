65e7de25dd85f24037a27e8d16bbea2d3c85b026
Patryk Lenart <deleteman136@gmail.com>
2021-10-09 14:48:41 +0200

    initial commit

---
+++ main.js
@@ -0,0 +1,6 @@
+const firstName = 'Patryk '
+const age = '19'
+
+const livingPlace = 'Suchej Beskidzkiej'
+console.log(`Siema jestem ${firstName}, mam ${age} lat i mieszkam w ${livingPlace}`)
+alert ('Jebać policje')
