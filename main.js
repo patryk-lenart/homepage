4c2248b33dee3472ad86b574e1854063013fae2e
Patryk Lenart <deleteman136@gmail.com>
2022-09-14 21:15:42 +0200

    js fun

--- main.js
+++ main.js
@@ -1,8 +1,67 @@
-alert("Kuba to kurwa");
-const imie = "Patryk";
+const firstName = "Patryk";
 const age = 20;
 
-console.log(imie);
-console.log(age);
-console.log("hejoo");
-console.log(`nazywam się ${imie} i mam ${age} lat.`);
+console.log(`nazywam się ${firstName} i mam ${age} lat.`);
+
+const heading = document.querySelector(".kuba");
+console.log(heading);
+
+const test = document.querySelector(".jp");
+test.innerHTML = `nazywam się ${firstName} i mam ${age} lat.`;
+
+const deathStar = {
+  diameter: 21150,
+  name: `Death Star`,
+  fire: (target) => {
+    console.log(`${target} destroyed`);
+  },
+  isOperating: true,
+  population: 21818,
+  commander: {
+    name: `Darth Vader`,
+    age: 69,
+  },
+};
+console.log(deathStar);
+deathStar.fire(`rebel ship`);
+console.log(deathStar.commander);
+
+const humanOne = {
+  name: "patryk",
+  age: 25,
+};
+const humanTwo = {
+  name: "kuba",
+  age: 12,
+};
+console.log(humanOne);
+console.log(humanTwo);
+
+console.log(humanTwo);
+humanOne.age = 30;
+humanTwo.age = humanOne.age;
+console.log(humanOne);
+console.log(humanTwo);
+
+function calculate(myNumber) {
+  return myNumber * 5;
+}
+const myResult = calculate(6);
+console.log(`musiałem pomnożyć moj numer przez 5 i otrzymałem ${myResult}`);
+
+function greet(name, age) {
+  console.log(`hej nazywam się ${name} i mam ${age} lat`);
+}
+greet(`huj`, 89393);
+
+const tank = {
+  name: `tiger`,
+  age: 20,
+  driver: {
+    name: "piotr",
+    age: 16,
+  },
+  rozpierdol(myTarget) {
+    console.log(`${myTarget} został rozpierdolony 💣`);
+  },
+};
